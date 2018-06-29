c298210d27ef85ccd668abf7d7f1f2f8bb3b58a7
Grzegorz Gronek <gg635@wp.pl>
2018-06-29 19:36:54 +0200

    test3 green

--- src/stringCalculatorKata/Calculator.java
+++ src/stringCalculatorKata/Calculator.java
@@ -3,10 +3,15 @@ package stringCalculatorKata;
 public class Calculator {
 
     public int calculate(String input) {
+        String[] numbers = input.split(",");
         if (isEmpty(input)) {
             return 0;
         }
-        return stringToInt(input);
+        if (input.length() == 1) {
+            return stringToInt(input);
+        } else {
+            return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+        }
     }
 
     private boolean isEmpty(String input) {
