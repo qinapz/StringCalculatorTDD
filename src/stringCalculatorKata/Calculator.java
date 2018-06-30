61d8180dfc3847479a776af38410c6b550ee1d04
Grzegorz Gronek <gg635@wp.pl>
2018-06-30 11:51:24 +0200

    test2 refactor

--- src/stringCalculatorKata/Calculator.java
+++ src/stringCalculatorKata/Calculator.java
@@ -3,10 +3,18 @@ package stringCalculatorKata;
 public class Calculator {
 
     public int calculate(String input) {
-        if (input.isEmpty()) {
+        if (isEmpty(input)) {
             return 0;
         }
+        return stringToInt(input);
+    }
+
+    private int stringToInt(String input) {
         return Integer.parseInt(input);
+    }
+
+    private boolean isEmpty(String input) {
+        return input.isEmpty();
     }
 
 }
