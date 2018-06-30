69bba2ec01ef45a57918b1974d886ddaf5a0e074
Grzegorz Gronek <gg635@wp.pl>
2018-06-30 11:48:37 +0200

    test1 refactor

--- src/stringCalculatorKataTest/TestCalculate.java
+++ src/stringCalculatorKataTest/TestCalculate.java
@@ -2,15 +2,22 @@ package stringCalculatorKataTest;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import stringCalculatorKata.Calculator;
 
 public class TestCalculate {
 
+    private Calculator calculator;
+
+    @Before
+    public void init() {
+        calculator = new Calculator();
+    }
+
     @Test
     public void emptyStringReturnsZero() {
-        Calculator calculator = new Calculator();
         assertEquals(0, calculator.calculate(""));
     }
 
