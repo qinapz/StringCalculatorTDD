320e3b68c934455dab2bffa8c14f8ef23b27c62e
Grzegorz Gronek <gg635@wp.pl>
2018-06-30 11:59:07 +0200

    test3 refactor

--- src/stringCalculatorKata/Calculator.java
+++ src/stringCalculatorKata/Calculator.java
@@ -2,16 +2,26 @@ package stringCalculatorKata;
 
 public class Calculator {
 
+    private static final String SEPARATOR = ",";
+
     public int calculate(String input) {
-        String[] numbers = input.split(",");
+        String[] numbers = input.split(SEPARATOR);
         if (isEmpty(input)) {
             return 0;
         }
-        if (input.length() == 1) {
+        if (isSingleNumber(input)) {
             return stringToInt(input);
         } else {
-            return stringToInt(numbers[0]) + stringToInt(numbers[1]);
+            return sum(numbers);
         }
+    }
+
+    private int sum(String[] numbers) {
+        return stringToInt(numbers[0]) + stringToInt(numbers[1]);
+    }
+
+    private boolean isSingleNumber(String input) {
+        return input.length() == 1;
     }
 
     private int stringToInt(String input) {
